0de580f1804e4e5f823902e6d5c1ec1fa189f8b3
dkstlduddla <dkstldud12345@naver.com>
2025-03-13 22:20:14 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,23 +1,3 @@
-x,y=map(int,input().split())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
-answer=False
-for i in range(y):
-    for j in range(x):
-        if b[i]==a[j]:
-            c=1
-            for z in range(y):
-                if b[i+z]==a[j+z]:
-                    c+=1
-            if c==y:
-                answer=True
-                break
-        if answer:
-            break
-    if answer:
-        break
-if answer:
-    print("Yes")
-else:
-    print("No")
-                
+컴퓨터공학부
+202402918   
+안시영
